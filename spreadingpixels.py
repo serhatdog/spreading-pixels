8c9376f01bd30f9636c1cd656f7719c7561e5e29
Serhat Dogan <85076922+serhatdog@users.noreply.github.com>
2022-07-15 03:35:32 +0300

    New function added
    Testing the function...

--- spreadingpixels.py
+++ spreadingpixels.py
@@ -2,11 +2,19 @@ import numpy as np
 from numba import jit
 
 @jit
-def resize(oI, size):
+def resize_up(oI, size):
     pI = np.zeros((len(oI)*size, len(oI[0])*size, 3), np.uint8)
-    for x in range(len(pI)):
-        for y in range(len(pI[0])):
+    for y in range(0, len(pI), 1):
+        for x in range(0, len(pI[0]), 1):
             pI[y][x] = oI[y//size][x//size]
+    return pI
+
+@jit
+def resize_down(oI, size):
+    pI = np.zeros((len(oI)//size, len(oI[0])//size, 3), np.uint8)
+    for y in range(0, len(oI)):
+        for x in range(0, len(oI[0])):
+            pI[y//size][x//size] = oI[y][x]
     return pI
 
 #Author: Serhat Dogan
