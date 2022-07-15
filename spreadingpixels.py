776118eca9cc365aa978730b02a9b66da992279d
Serhat Dogan <85076922+serhatdog@users.noreply.github.com>
2022-07-15 15:53:30 +0300

    Function improved

--- spreadingpixels.py
+++ spreadingpixels.py
@@ -2,20 +2,26 @@ import numpy as np
 from numba import jit
 
 @jit
-def resize_up(oI, size):
-    pI = np.zeros((len(oI)*size, len(oI[0])*size, 3), np.uint8)
-    for y in range(0, len(pI), 1):
-        for x in range(0, len(pI[0]), 1):
-            pI[y][x] = oI[y//size][x//size]
-    return pI
+def resize(oI, size):
+    
+    if size > 0:
 
-@jit
-def resize_down(oI, size):
-    pI = np.zeros((len(oI)//size, len(oI[0])//size, 3), np.uint8)
-    for y in range(0, len(oI)):
-        for x in range(0, len(oI[0])):
-            pI[y//size][x//size] = oI[y][x]
-    return pI
+        pI = np.zeros((len(oI)*size, len(oI[0])*size, 3), np.uint8)
+        for y in range(0, len(pI), 1):
+            for x in range(0, len(pI[0]), 1):
+                pI[y][x] = oI[y//size][x//size]
+        return pI
+
+    elif size < 0:
+        
+        size *= -1 
+        pI = np.zeros((len(oI)//size, len(oI[0])//size, 3), np.uint8)
+        for y in range(0, len(oI)):
+            for x in range(0, len(oI[0])):
+                pI[y//size][x//size] = oI[y][x]
+        return pI
+
+    return oI
 
 #Author: Serhat Dogan
 #https://github.com/serhatdog/spreading-pixels
