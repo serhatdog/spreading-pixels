07c9696710d60a8d8543675d904d2b1b8d5792ad
Serhat Dogan <85076922+serhatdog@users.noreply.github.com>
2022-10-15 19:00:35 +0300

    Conditions are fixed
    size conditions changed by 1 and -1

--- spreadingpixels.py
+++ spreadingpixels.py
@@ -3,13 +3,13 @@ from numba import jit
 
 @jit
 def resize(img, size):
-    if size > 0:
+    if size > 1:
         output_img = zeros((len(img)*size, len(img[0])*size, 3), uint8)
         for y in range(len(output_img)):
             for x in range(len(output_img[0])):
                 output_img[y][x] = img[y//size][x//size]
         return output_img;
-    elif size < 0:
+    elif size < -1:
         size*=-1
         output_img = zeros((len(img)//size, len(img[0])//size, 3), uint8)
         for y in range(len(output_img)):
